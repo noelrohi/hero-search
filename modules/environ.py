56616236f6c384ea523d7f042a9b723f45d07ea5
gneiru <noelrohi59@gmail.com>
2023-03-15 09:11:48 +0800

    chore: remove mongo from dotenv

--- modules/environ.py
+++ modules/environ.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 prefix = os.environ.get("PREFIX")
-mongo_uri = os.environ.get("MONGO_URI")
+# mongo_uri = os.environ.get("MONGO_URI")
 token = os.environ.get("TOKEN")
 api = os.environ.get("API")
